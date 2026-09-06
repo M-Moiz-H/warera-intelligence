3b6736a750a4c9e720099dd36edae20c9d87e201
M-Moiz-H <m.moiz.h.0000@gmail.com>
2026-09-06 09:23:38 +0500

    Delete src/warera/provider-manager.ts

--- src/warera/provider-manager.ts
+++
@@ -1,7 +0,0 @@
-import type {WarEraProvider} from "./provider.js";
-export class ProviderManager{
- constructor(public readonly provider:WarEraProvider){}
- async healthy(){
-  return this.provider.healthCheck()
- }
-}
